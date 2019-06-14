077395e1c604a53853b65d780d5a1f33f1cb5d89
Romy <berniromy@hotmail.com>
2019-06-14 16:48:32 -0300

    FAQ + home.php en vez de home.html + footer+ header

---
+++ FAQ.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Obstgarten</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="RB_home_estilo_v20190608.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+  <body>
+
+<?php include_once ("header.html") ?>
+    <div class="accordion" id="accordionExample">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              ¿Cuales son los medios de pago aceptados?
+            </button>
+          </h2>
+        </div>
+
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+          <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              ¿Se pueden devolver los articulos ya comprados?
+            </button>
+          </h2>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+          <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header" id="headingThree">
+          <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              ¿Cómo puedo acceder a exponer mis artículos en la feria?
+            </button>
+          </h2>
+        </div>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+          <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include_once ("footer.html") ?>
+
+  </body>
+</html>
---
+++ RB_home_v20190614.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Obstgarten</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="RB_home_estilo_v20190608.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  </head>
+  <body>
+<header class="main-header">
+  <img src="Obstgarten_wix_logo_maker.png" alt="logo" class="logo">
+
+
+
+<nav class="main-nav">
+
+  <a href="#" class="toggle-nav">
+  <span class='fa fa-bars'></span>
+  </a>
+
+  <ul>
+    <li> <a href="#">Home</a></li>
+    <li><a href="#">Productos frescos</a></li>
+    <li><a href="#">Productos envasados</a></li>
+    <li><a href="#">Locaciones</a></li>
+    <li><a href="#">Nuestra Historia</a></li>
+    <li><a href="#">Contactenos</a></li>
+  </ul>
+
+</nav>
+
+</header>
+
+<section class='banner'>
+  <img src="photo-vegetales.jpg" alt="fotobanner">
+  </section>
+
+<section class="vip-products">
+  <article class="product">
+    <div class="photo-container">
+      <img class="photo" src="parking-zone-design.jpg" alt="parking">
+      <a class="zoom" href="#"></a>
+    </div>
+  </article>
+  <h2>Estacionamiento</h2>
+  <p>Con su compra superior a $300, puede acceder a un estacionamiento gratuito. <a href="#">Ver mas</a></p>
+</section>
+
+<section class="vip-products">
+  <article class="product">
+    <div class="photo-container">
+      <iframe class="photo" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13393.83395761976!2d-60.7387922!3d-32.9389025!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x97be5ba20d2322cf!2sMercado+de+Concentraci%C3%B3n+Fisherton!5e0!3m2!1ses-419!2sar!4v1560025104802!5m2!1ses-419!2sar" width="300" height="220" frameborder="0" style="border:0" allowfullscreen></iframe>
+      <a class="zoom" href="#"></a>
+    </div>
+  </article>
+  <h2>Como llegar</h2>
+  <p>Para obtener indicaciones en tiempo real del tránsito presione click aquí. <a href="#">Ver mas</a></p>
+
+
+</section>
+
+<section class="vip-products">
+  <article class="product">
+    <div class="photo-container">
+      <img class="fotoquenoresponde" src="senior-woman-holding-box-vegetables.jpg" alt="productores">
+      <a class="zoom" href="#"></a>
+    </div>
+  </article>
+  <h2>¿Quiere visitar a los productores?</h2>
+  <p>Se organizan visitas a pedido de los clientes, con nuestros productores. Para coordinarlas contáctenos. <a href="#">Ver mas</a></p>
+
+
+</section>
+
+<?php
+include_once "footer.html" ?>
+
+
+  </body>
+</html>
