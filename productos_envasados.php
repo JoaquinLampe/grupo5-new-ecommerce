cc83280602bccd698c97af8926dfa79678c293e4
JoaquinLampe <50156919+JoaquinLampe@users.noreply.github.com>
2019-06-29 16:09:25 -0300

    primer intento listado de productos. solo mobile

---
+++ productos_envasados.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Obstgarten</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styleUnico.css">
+  </head>
+    <body>
+      <?php include_once ("RB_header.html") ?>
+      <section class="productos">
+
+        <article class="producto">
+          <div class="display">
+            <div class="fondo-organic"></div>
+            <img class="best" src="img/bestseller.png" alt="">
+            <img class="organic" src="img/100organic.png" alt="">
+            <img class="foto-producto"src="img/prod1.jpg" alt="">
+          </div>
+          <h2>
+            Aliquam tincidunt mauris eu risus.
+          </h2>
+          <p class="precio">
+          $9.90
+          </p>
+          <p>
+            Cras ornare tristique elit.
+          </p>
+          <div class="botones-producto">
+            <button class="btn btn-outline-primary ver-mas">
+              Ver Mas
+            </button>
+            <button class="btn btn-outline-secondary al-carrito">
+              <i class="fas fa-shopping-cart"></i>
+            </button>
+          </div>
+        </article>
+
+        <article class="producto">
+          <div class="display">
+            <img class="organic" src="img/100organic.png" alt="">
+            <img class="foto-producto"src="img/prod2.jpg" alt="">
+          </div>
+          <h2>
+            Aliquam tincidunt mauris eu risus.
+          </h2>
+          <p class="precio">
+          $9.90
+          </p>
+          <p>
+            Cras ornare tristique elit.
+          </p>
+          <div class="botones-producto">
+            <button class="btn btn-outline-primary ver-mas">
+              Ver Mas
+            </button>
+            <button class="btn btn-outline-secondary al-carrito">
+              <i class="fas fa-shopping-cart"></i>
+            </button>
+          </div>
+        </article>
+
+        <article class="producto">
+          <div class="display">
+            <img class="organic" src="img/100organic.png" alt="">
+            <img class="foto-producto"src="img/prod3.jpg" alt="">
+          </div>
+          <h2>
+            Aliquam tincidunt mauris eu risus.
+          </h2>
+          <p class="precio">
+          $9.90
+          </p>
+          <p>
+            Cras ornare tristique elit.
+          </p>
+          <div class="botones-producto">
+            <button class="btn btn-outline-primary ver-mas">
+              Ver Mas
+            </button>
+            <button class="btn btn-outline-secondary al-carrito">
+              <i class="fas fa-shopping-cart"></i>
+            </button>
+          </div>
+        </article>
+
+        <article class="producto">
+          <div class="display">
+            <img class="organic" src="img/100organic.png" alt="">
+            <img class="foto-producto"src="img/prod4.jpg" alt="">
+          </div>
+          <h2>
+            Aliquam tincidunt mauris eu risus.
+          </h2>
+          <p class="precio">
+          $9.90
+          </p>
+          <p>
+            Cras ornare tristique elit.
+          </p>
+          <div class="botones-producto">
+            <button class="btn btn-outline-primary ver-mas">
+              Ver Mas
+            </button>
+            <button class="btn btn-outline-secondary al-carrito">
+              <i class="fas fa-shopping-cart"></i>
+            </button>
+          </div>
+        </article>
+
+        <article class="producto">
+          <div class="display">
+            <img class="organic" src="img/100organic.png" alt="">
+            <img class="foto-producto"src="img/prod5.jpg" alt="">
+          </div>
+          <h2>
+            Aliquam tincidunt mauris eu risus.
+          </h2>
+          <p class="precio">
+          $9.90
+          </p>
+          <p>
+            Cras ornare tristique elit.
+          </p>
+          <div class="botones-producto">
+            <button class="btn btn-outline-primary ver-mas">
+              Ver Mas
+            </button>
+            <button class="btn btn-outline-secondary al-carrito">
+              <i class="fas fa-shopping-cart"></i>
+            </button>
+          </div>
+        </article>
+
+        <article class="producto">
+          <div class="display">
+            <img class="organic" src="img/100organic.png" alt="">
+            <img class="foto-producto"src="img/prod6.jpg" alt="">
+          </div>
+          <h2>
+            Aliquam tincidunt mauris eu risus.
+          </h2>
+          <p class="precio">
+          $9.90
+          </p>
+          <p>
+            Cras ornare tristique elit.
+          </p>
+          <div class="botones-producto">
+            <button class="btn btn-outline-primary ver-mas">
+              Ver Mas
+            </button>
+            <button class="btn btn-outline-secondary al-carrito">
+              <i class="fas fa-shopping-cart"></i>
+            </button>
+          </div>
+        </article>
+      </section>
+      <?php include_once ("RB_footer.html") ?>
+    </body>
+  </html>
